a420ad68991e72995ee6107de71daae8f4ae0501
Kevin Harrington <mad.hephaestus@gmail.com>
2023-02-09 11:43:20 -0500

    adding names

--- union.groovy
+++ union.groovy
@@ -5,8 +5,10 @@ CSG cube = new Cube(	size,// X dimention
 			).toCSG()
 //create a sphere
 CSG sphere = new Sphere(size/20*12.5).toCSG()
+sphere.setName("sphere")
 // perform a union
 CSG cubePlusSphere = cube.union(sphere);
+cubePlusSphere.setName("cubePlusSphere") 
 
 //To union a list of CSG's together use the static unioAll
 CSG allUnion = CSG.unionAll([cubePlusSphere , cube.movex(size*1.5), sphere.movey(size*1.5)])
