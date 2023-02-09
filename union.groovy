b21d1941ca0cd4aed6d73d9c2b84ca954f80c247
Kevin Harrington <mad.hephaestus@gmail.com>
2023-02-09 11:44:03 -0500

    names

--- union.groovy
+++ union.groovy
@@ -3,6 +3,7 @@ CSG cube = new Cube(	size,// X dimention
 			size,// Y dimention
 			size//  Z dimention
 			).toCSG()
+cube.setName("cube")		
 //create a sphere
 CSG sphere = new Sphere(size/20*12.5).toCSG()
 sphere.setName("sphere")
